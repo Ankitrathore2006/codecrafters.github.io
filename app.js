20e65f37f6c13e8978272c97a97a50169083db04
Ankit Rathore <ankitrathore4310@gmail.com>
2025-02-21 00:23:29 +0530

    update hero section

--- app.js
+++ app.js
@@ -9,31 +9,44 @@ nextBtn.onclick = () => {
     active = active + 1;
     setSlider();
 }
+
 prevBtn.onclick = () => {
     active = active - 1;
     setSlider();
 }
+
 const setSlider = () => {
     let oldActive = document.querySelector('.slider .list .item.active');
     if(oldActive) oldActive.classList.remove('active');
     items[active].classList.add('active');
-    // 
+    // Show/Hide buttons based on active position
     nextBtn.classList.remove('d-none');
     prevBtn.classList.remove('d-none');
     if(active == lastPosition) nextBtn.classList.add('d-none');
     if(active == firstPosition) prevBtn.classList.add('d-none');
 }
+
 setSlider();
 
-// set diameter
+// Set diameter
 const setDiameter = () => {
     let slider = document.querySelector('.slider');
     let widthSlider = slider.offsetWidth;
     let heightSlider = slider.offsetHeight;
     let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
-    document.documentElement.style.setProperty('--diameter', diameter+'px');
+    document.documentElement.style.setProperty('--diameter', diameter + 'px');
 }
+
 setDiameter();
 window.addEventListener('resize', () => {
     setDiameter();
 })
+
+// Automatic slider transition every 5 seconds
+setInterval(() => {
+    active = active + 1;
+    if (active > lastPosition) {
+        active = firstPosition; // Reset to first slide after last one
+    }
+    setSlider();
+}, 7000); // seconds interval
